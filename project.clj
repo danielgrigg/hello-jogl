783621d2b789db70d6544a01c2a0cf6086e54ef6
Daniel Casimir Grigg <mail@danielgrigg.com>
2013-04-03 22:36:53 +1100

    Messing around still.

--- project.clj
+++ project.clj
@@ -4,6 +4,10 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [local/jogl-all "1.0.0"]]
+                 [org.clojars.toxi/jogl "2.0.0-rc10"]]
+;                 [local/gluegen-rt-natives-macosx-universal "1.0.0"]
+;                 [local/gluegen-rt "1.0.0"]
+;                 [local/jogl-all "1.0.0"]]
   :repositories {"local" ~(str (.toURI (java.io.File. "maven-repo")))}
+  :native-path "lib/jogamp-all-platforms/lib/"
   :main hello-jogl.core)
--- src/hello_jogl/core.clj
+++ src/hello_jogl/core.clj
@@ -1,5 +1,18 @@
 (ns hello-jogl.core
-  (:gen-class))
+  (:gen-class)
+  (:import (java.awt Frame))
+  (:import (javax.media.opengl.awt GLCanvas)))
+
+;;(. System setProperty "java.library.path" ".:/Library/Java/Extensions:/System/Library/Java/Extensions:/usr/lib/jav:lib/jogamp-all-platforms/lib/")
+
+(def ^:dynamic canvas (GLCanvas.))
+(def ^:dynamic frame (Frame.))
+
+(defn main []
+  (do
+   nil
+  )
+)
 
 (defn -main
   "I don't do a whole lot ... yet."
